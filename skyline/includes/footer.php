fd7835389d443810c334ece90db21383d9ab31fc
Virushd1000 <77389788+Virushd1000@users.noreply.github.com>
2022-08-23 23:42:47 +0100

    Add files via upload

---
+++ skyline/includes/footer.php
@@ -0,0 +1,156 @@
+<style type="text/css">
+	.foot{
+		padding: 50px;
+		background: #000000;
+	}
+	.foot_u{
+		list-style: none;
+		color: #d5d5d5;
+	}
+	.foot_p{
+		font-size: 25px;
+		text-transform: capitalize;
+	}
+	.foot_l{
+		margin-top: 10px;
+	}
+	.foot_a{
+		font-size: 18px;
+		text-decoration: none;
+		color: #acacac;
+	}
+	.foot_a:hover{
+		text-decoration: none;
+		color: #acacac;
+	}
+	.foot_line{
+		width: 2px;
+		height: 20px;
+		background: #777;
+		position: relative;
+		bottom: 20px;
+	}
+	.foot_uu{
+		display: inline-flex;
+		list-style: none;
+	}
+	.foot_ll{
+		margin-left: 20px;
+	}
+	.foot_uuu{
+		float: right;
+		display: inline-flex;
+		list-style: none;
+		margin-left: 430px;
+	}
+	.foot_fa{
+		font-size: 25px;
+	}
+	@media(max-width: 800px){
+	/**Footer**/
+	.foot{
+		padding: 20px;
+	}
+	.foot_p{
+		font-size: 20px;
+	}
+	.foot_l{
+		margin-top: 10px;
+	}
+	.foot_a{
+		font-size: 16px;
+	}
+	.foot_a:hover{
+		text-decoration: none;
+		color: #acacac;
+	}
+	.foot_line{
+		display: none;
+	}
+	.foot_uu{
+		display: block;
+	}
+	.foot_ll{
+		margin-left: 0px;
+		margin-top: 10px;
+	}
+	.foot_uuu{
+		float: none;
+		margin-left: 0px;
+	}
+	.foot_fa{
+		font-size: 20px;
+		margin-left: 10px;
+	}
+	}
+</style>
+	<footer class="foot">
+		<div class="container">
+			<div>
+				<div class="row">
+					<div class="col-sm-12 col-md-4 col-lg-4">
+						<ul class="foot_u">
+							<div>
+								<p class="foot_p">Browse</p>
+							</div>
+							<div class="row">
+								<div class="col-sm-12 col-md-6 col-lg-6">
+									<li class="foot_l"><a href="" class="foot_a">Streaming Library</a></li>
+									<li class="foot_l"><a href="" class="foot_a">Live Streaming</a></li>
+									<li class="foot_l"><a href="" class="foot_a">News</a></li>
+									<li class="foot_l"><a href="" class="foot_a">iReport</a></li>
+								</div>
+								<div class="col-sm-12 col-md-6 col-lg-6">
+									<li class="foot_l"><a href="" class="foot_a">TV Shows</a></li>
+									<li class="foot_l"><a href="" class="foot_a">Movies</a></li>
+									<li class="foot_l"><a href="" class="foot_a">Coming Soon</a></li>
+									<li class="foot_l"><a href="" class="foot_a">Trending</a></li>
+								</div>
+							</div>
+						</ul>
+					</div>
+					<div class="col-sm-12 col-md-4 col-lg-4">
+						<ul class="foot_u">
+							<div>
+								<p class="foot_p">help center</p>
+							</div>
+							<li class="foot_l"><a href="" class="foot_a">Account & Billing</a></li>
+							<li class="foot_l"><a href="" class="foot_a">Pricing & Plans</a></li>
+							<li class="foot_l"><a href="" class="foot_a">Supported Devices</a></li>
+							<li class="foot_l"><a href="" class="foot_a">Accessibility</a></li>
+						</ul>
+					</div>
+					<div class="col-sm-12 col-md-4 col-lg-4">
+						<ul class="foot_u">
+							<div>
+								<p class="foot_p">contact us</p>
+							</div>
+							<li class="foot_l"><a href="" class="foot_a">Address</a></li>
+							<li class="foot_l"><a href="" class="foot_a">Email</a></li>
+							<li class="foot_l"><a href="" class="foot_a">Phone</a></li>
+							<li class="foot_l"><a href="" class="foot_a">24/7 Chat</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div>
+				<hr>
+			</div>
+			<div>
+				<ul class="foot_uu">
+					<li class="foot_ll"><a href="" class="foot_a">© 2022 Copyright</a></li>
+					<li class="foot_ll"><hr class="foot_line"></li>
+					<li class="foot_ll" class="foot_ll"><a href="" class="foot_a">Term of Use</a></li>
+					<li class="foot_ll"><hr class="foot_line"></li>
+					<li class="foot_ll"><a href="" class="foot_a">Privacy Policy</a></li>
+					<li class="foot_ll">
+						<ul class="foot_uuu">
+							<li class="foot_ll"><a href="" class="foot_a"><i class="fa fa-facebook-official foot_fa" style="color: #5223ff;"></i></a></li>
+							<li class="foot_ll"><a href="" class="foot_a"><i class="fa fa-instagram foot_fa" style="color: #7a1334;"></i></a></li>
+							<li class="foot_ll"><a href="" class="foot_a"><i class="fa fa-twitter foot_fa" style="color: #259eb8;"></i></a></li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</footer>
