c2d76522adb6e8f820b644c199e797d72c161ecb
ayobami ajibola <ayobamiajibola46@gmail.com>
2022-10-05 13:24:21 +0100

    test

--- server/src/index.jsx
+++ server/src/index.jsx
@@ -8,6 +8,10 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, "../../client", "build")));
 
+app.get("/welcome", (req, res) => {
+    res.status(200).send({ data: "Welcome Skyline"});
+  });
+
 const port = 8080;
 
 app.listen(port, () => {
